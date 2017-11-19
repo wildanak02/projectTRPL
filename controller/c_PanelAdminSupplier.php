06430590856ec4878df64aa64493a08cbfdbe490
beya <beyaajibuana@gmail.com>
2017-11-19 20:28:25 +0700

    adminSupplier(tambah+delete)

--- controller/c_PanelAdminSupplier.php
+++ controller/c_PanelAdminSupplier.php
@@ -3,22 +3,33 @@ Class PanelAdminSupplierController{
 
 	public function home(){
 		$posts=AdminSupplier::viewSupplier();
-		require_once("view/v_ModalAdmin.php");
 		require_once("view/v_PanelAdminSupplier.php");
 	}
 
 
-  public function tambahSupplier(){
-				$user= AdminSupplier::tambahSupplier($_POST["namaSupplier"],$_POST["alamat"]);
-				header("location:index.php?controller=PanelAdminSupplier&action=home");
+	public function klikTambah(){
+		require_once("view/v_AdminSupplierTambah.php");
 	}
 
-  public function editSupplier(){
-
+	public function klikEdit(){
+		$posts=AdminSupplier::ambiledit($_GET["idSupplier"]);
+		require_once("view/v_AdminSupplierEdit.php");
 	}
 
-  public function hapusSupplier(){
+	public function tambahSupplier(){
+		$user= AdminSupplier::tambahSupplier($_POST["namaSupplier"],$_POST["alamat"]);
+		header("location:index.php?controller=PanelAdminSupplier&action=home");
+	}
 
+	public function editSupplier(){
+		
+		$posts=AdminSupplier::edit($_GET["idSupplier"],$_GET["namaSupplier"],$_GET["alamat"]);
+			header("location:index.php?controller=PanelAdminSupplier&action=home");
+		} 
+
+	public function hapusSupplier(){
+		$posts=AdminSupplier::hapus($_GET["idSupplier"]);
+		header("location:index.php?controller=PanelAdminSupplier&action=home");
 	}
 }
 
